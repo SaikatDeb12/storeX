9dd87943a7593a418081acfcef321c0bd665f63a
Saikatdeb12 <saikat.rcld@gmail.com>
2026-02-27 18:15:27 +0530

    transaction working

--- internal/handlers/asset.handler.go
+++ internal/handlers/asset.handler.go
@@ -28,30 +28,33 @@ func CreateAsset(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	assetID, err := dbhelper.CreateAsset(req)
+	err := database.Tx(func(tx *sqlx.Tx) error {
+		assetID, err := dbhelper.CreateAsset(req)
+		if err != nil {
+			return err
+		}
+
+		assetType := req.Type
+		switch assetType {
+		case "laptop":
+			err = dbhelper.InsertLaptopDetails(assetID, *req.Laptop)
+		case "keyboard":
+			err = dbhelper.InsertKeyboardDetails(assetID, *req.Keyboard)
+		case "mouse":
+			err = dbhelper.InsertMouseDetails(assetID, *req.Mouse)
+		case "mobile":
+			err = dbhelper.InsertMobileDetails(assetID, *req.Mobile)
+		default:
+			err = errors.New("invalid asset type")
+		}
+
+		return err
+	})
 	if err != nil {
 		utils.RespondError(w, http.StatusInternalServerError, err, "failed to create asset")
 		return
 	}
 
-	assetType := req.Type
-	switch assetType {
-	case "laptop":
-		err = dbhelper.InsertLaptopDetails(assetID, *req.Laptop)
-	case "keyboard":
-		err = dbhelper.InsertKeyboardDetails(assetID, *req.Keyboard)
-	case "mouse":
-		err = dbhelper.InsertMouseDetails(assetID, *req.Mouse)
-	case "mobile":
-		err = dbhelper.InsertMobileDetails(assetID, *req.Mobile)
-	default:
-		err = errors.New("invalid asset type")
-	}
-
-	if err != nil {
-		utils.RespondError(w, http.StatusInternalServerError, err, "")
-		return
-	}
 	utils.RespondJSON(w, http.StatusCreated, map[string]string{
 		"message": "asset added",
 	})
--- internal/handlers/auth.handler.go
+++ internal/handlers/auth.handler.go
@@ -3,10 +3,12 @@ package handler
 import (
 	"net/http"
 
+	"github.com/SaikatDeb12/storeX/internal/database"
 	"github.com/SaikatDeb12/storeX/internal/database/dbhelper"
 	"github.com/SaikatDeb12/storeX/internal/middleware"
 	"github.com/SaikatDeb12/storeX/internal/models"
 	"github.com/SaikatDeb12/storeX/internal/utils"
+	"github.com/jmoiron/sqlx"
 )
 
 func Register(w http.ResponseWriter, r *http.Request) {
@@ -38,20 +40,25 @@ func Register(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	userID, err := dbhelper.CreateUser(req.Name, req.Email, req.PhoneNumber, req.Role, req.Employment, hashedPassword)
-	if err != nil {
-		utils.RespondError(w, http.StatusInternalServerError, err, "failed to create user")
-		return
-	}
+	var token string
+	err = database.Tx(func(tx *sqlx.Tx) error {
+		userID, err := dbhelper.CreateUser(req.Name, req.Email, req.PhoneNumber, req.Role, req.Employment, hashedPassword)
+		if err != nil {
+			utils.RespondError(w, http.StatusInternalServerError, err, "failed to create user")
+			return err
+		}
 
-	sessionID, err := dbhelper.CreateSession(userID)
-	if err != nil {
-		utils.RespondError(w, http.StatusInternalServerError, err, "failed to create session")
-		return
-	}
+		sessionID, err := dbhelper.CreateSession(userID)
+		if err != nil {
+			utils.RespondError(w, http.StatusInternalServerError, err, "failed to create session")
+			return err
+		}
 
-	role := req.Role
-	token, err := utils.GenerateJWT(userID, sessionID, role)
+		role := req.Role
+		token, err = utils.GenerateJWT(userID, sessionID, role)
+
+		return err
+	})
 	if err != nil {
 		utils.RespondError(w, http.StatusInternalServerError, err, "error while generating token")
 		return
